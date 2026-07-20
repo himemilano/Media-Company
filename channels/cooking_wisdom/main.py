4da5e22e2d3e424430bb0aa6ffbc2a6d922d3f18
himemilano <matsumatsuceo@gmail.com>
2026-07-20 13:47:28 +0900

    Update main.py

--- channels/cooking_wisdom/main.py
+++ channels/cooking_wisdom/main.py
@@ -31,7 +31,7 @@ except ImportError:
 jst = timezone(timedelta(hours=9))
 current_date = datetime.now(jst).strftime("%Y-%m-%d")
 
-TEMPLATE_DIR = "../../templates"
+TEMPLATE_DIR = "../../templates/cooking_wisdom"
 WORKSPACE_DIR = "workspace"      
 TEMP_DIR = "temp_assets"
 
